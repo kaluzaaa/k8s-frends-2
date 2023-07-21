5f6286f1768abde392b4bb317f68c6f8ca2a7bce
Lukasz Kaluzny <lukasz@kaluzny.pro>
2023-07-21 08:27:14 +0000

    Add cpu endpoint

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -6,5 +10,11 @@ app.Urls.Add("http://*:3000");
 app.MapGet("/test", () => app.Configuration["test"]);
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/cpu", () => {
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    Thread.SpinWait(100*1000000);
+    return stopwatch.Elapsed.TotalMilliseconds;
+});
 
 app.Run();
